164ceeddf76d6abc2dd925cf452cf0a85559f514
Anna Schneider <anna.schneiderm@gmail.com>
2020-12-07 18:36:46 -0500

    Imported client, added Shopify components

--- needful-things/src/index.js
+++ needful-things/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import Client from "shopify-buy"
+
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+
+const client = Client.buildClient({
+	storefrontAccessToken: "REACT_APP_SHOPIFY_KEY",
+	domain: "2020s-needful-things.myshopify.com",
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
