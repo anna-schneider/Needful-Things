5d03c4cbb95526803ea1b56e3dc1e3a40486f519
Anna Schneider <anna.schneiderm@gmail.com>
2020-12-07 18:36:46 -0500

    Imported client, added Shopify components

--- needful-things/src/App.js
+++ needful-things/src/App.js
@@ -2,6 +2,11 @@ import "./App.css"
 
 import Header from "./components/Header/Header"
 
+// let client = ShopifyBuy.buildClient({
+// 	domain: "2020s-Needful-Things.myshopify.com",
+// 	storefrontAccessToken: "REACT_APP_SHOPIFY_KEY",
+// })
+
 function App() {
 	return (
 		<div className="App">
