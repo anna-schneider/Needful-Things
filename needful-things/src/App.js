a6038e35806f11d3cb70e3af7b69022ab674c3f5
Anna Schneider <anna.schneiderm@gmail.com>
2020-12-07 21:58:35 -0500

    Populated Shopify components

--- needful-things/src/App.js
+++ needful-things/src/App.js
@@ -1,18 +1,39 @@
+import { Component } from "react"
 import "./App.css"
 
 import Header from "./components/Header/Header"
+import Cart from "./components/Shopify/Cart"
 
 // let client = ShopifyBuy.buildClient({
 // 	domain: "2020s-Needful-Things.myshopify.com",
 // 	storefrontAccessToken: "REACT_APP_SHOPIFY_KEY",
 // })
 
-function App() {
-	return (
-		<div className="App">
-			<Header />
-		</div>
-	)
+class App extends Component() {
+	constructor() {
+		super()
+		this.updateQuantityInCart = this.updateQuantityInCart.bind(this)
+		this.removeLineItemInCart = this.removeLineItemInCart.bind(this)
+		this.handleCartClose = this.handleCartClose.bind(this)
+		this.handleCartOpen = this.handleCartOpen.bind(this)
+	}
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<div>
+					...
+					<Cart
+						checkout={this.state.checkout}
+						isCartOpen={this.state.isCartOpen}
+						handleCartClose={this.handleCartClose}
+						updateQuantityInCart={this.updateQuantityInCart}
+						removeLineItemInCart={this.removeLineItemInCart}
+					/>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App
