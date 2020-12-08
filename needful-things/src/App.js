ebdb091ce6cb31e2c1402adecf76d49c421b073e
Anna Schneider <anna.schneiderm@gmail.com>
2020-12-07 22:39:58 -0500

    Added event handlers

--- needful-things/src/App.js
+++ needful-things/src/App.js
@@ -19,7 +19,42 @@ class App extends Component() {
 		this.handleCartClose = this.handleCartClose.bind(this)
 		this.handleCartOpen = this.handleCartOpen.bind(this)
 	}
+	updateQuantityInCart(lineItemId, quantity) {
+		const state = store.getState() // state from redux store
+		const checkoutId = state.checkout.id
+		const lineItemsToUpdate = [
+			{ id: lineItemId, quantity: parseInt(quantity, 10) },
+		]
+		state.client.checkout
+			.updateLineItems(checkoutId, lineItemsToUpdate)
+			.then((res) => {
+				store.dispatch({
+					type: "UPDATE_QUANTITY_IN_CART",
+					payload: { checkout: res },
+				})
+			})
+	}
+	removeLineItemInCart(lineItemId) {
+		const state = store.getState() // state from redux store
+		const checkoutId = state.checkout.id
+		state.client.checkout
+			.removeLineItems(checkoutId, [lineItemId])
+			.then((res) => {
+				store.dispatch({
+					type: "REMOVE_LINE_ITEM_IN_CART",
+					payload: { checkout: res },
+				})
+			})
+	}
+	handleCartClose() {
+		store.dispatch({ type: "CLOSE_CART" })
+	}
+	handleCartOpen() {
+		store.dispatch({ type: "OPEN_CART" })
+	}
 	render() {
+		//state from Redux store
+		const state = store.getState()
 		return (
 			<div className="App">
 				<Header />
